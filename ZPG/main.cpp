5f5d0f972be5e69e2b49d755b6cecfb6cd606205
Andreas <andreasbrudovskyy@gmail.com>
2026-09-01 08:17:55 +0200

    shadery, VBO, VAO

--- ZPG/main.cpp
+++ ZPG/main.cpp
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <cstdlib>
 
+
+
 static void error_callback(int error, const char* description){
     fprintf(stderr, "GLFW error %d: %s\n", error, description);
 }
@@ -46,6 +48,62 @@ static void window_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+const char* vertex_shader =
+"#version 330\n"
+"layout(location=0) in vec3 vp;"
+"void main () {"
+"     gl_Position = vec4 (vp, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 330\n"
+"out vec4 frag_colour;"
+"void main () {"
+"     frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+"}";
+
+float points[] = {
+     0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f,
+
+};
+
+
+static bool zkontrolujShader(GLuint shader, const char* jmeno)
+{
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Chyba prekladu shaderu (%s): %s\n", jmeno, strInfoLog);
+        delete[] strInfoLog;
+        return false;
+    }
+    return true;
+}
+
+static bool zkontrolujProgram(GLuint program)
+{
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Chyba linkovani programu: %s\n", strInfoLog);
+        delete[] strInfoLog;
+        return false;
+    }
+    return true;
+}
 
 int main()
 {
@@ -56,6 +114,11 @@ int main()
         fprintf(stderr, "ERROR: could not start GLFW3\n");
         return EXIT_FAILURE;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "ZPG", NULL, NULL);
     if (!window)
@@ -85,6 +148,36 @@ int main()
     glm::mat4 testM = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0, 0, 1));
     printf("GLM test (ma byt 0.707 0.707): %.3f %.3f\n", testM[0][0], testM[0][1]);
 
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+    glCompileShader(vertexShader);
+    if (!zkontrolujShader(vertexShader, "vertex")) return EXIT_FAILURE;
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+    if (!zkontrolujShader(fragmentShader, "fragment")) return EXIT_FAILURE;
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glLinkProgram(shaderProgram);
+    if (!zkontrolujProgram(shaderProgram)) return EXIT_FAILURE;
+
+
+    GLuint VBO = 0;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    GLuint VAO = 0;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
@@ -106,28 +199,13 @@ int main()
 
         const float px = 0.5f, py = 0.5f;
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-
-        glTranslatef(px, py, 0.0f);        // 3. posuň zpátky
-        glRotatef(angle, x,y,z);  // 2. zarotuj
-        glTranslatef(-px, -py, 0.0f);        // 1. posuň P do počátku
-
-        glBegin(GL_TRIANGLE_FAN);
-            glColor3f(1.0f, 0.0f, 0.0f);
-            glVertex3f(0.5f, 0.5f, 0.0f);
-
-            glColor3f(1.0f, 1.0f, 0.0f);
-            glVertex3f(0.5f, -0.5f, 0.0f);
-
-            glColor3f(0.0f, 1.0f, 0.0f);
-            glVertex3f(-0.5f, -0.5f, 0.0f);
-
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glVertex3f(-0.5f, 0.5f, 0.0f);
-        glEnd();
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
         glfwSwapBuffers(window);
+        GLenum e = glGetError();
+        if (e != GL_NO_ERROR) printf("GL error: 0x%x\n", e);
         glfwPollEvents();
     }
 
